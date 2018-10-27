dd29ac67e0ca7cf69a6b52cdcea26dd9d35a1f2c
navkal <navkal@hotmail.com>
2018-10-26 21:47:34 -0400

    Fix MHL agent

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   require_once $_SERVER["DOCUMENT_ROOT"] . "/../common/util.php";
 
   // Track client IP addresses
-  include $_SERVER['DOCUMENT_ROOT'] . '/ip/ip.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/clients/clients.php';
 
   // Determine whether to service bulk or single request
   require_once $_SERVER["DOCUMENT_ROOT"] . ( isset( $_REQUEST['bulk'] ) ? '/gb.php' : '/bg.php' );
