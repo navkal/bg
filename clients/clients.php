a2cab03ced214621afa81833a8b265acbdd9a380
navkal <navkal@hotmail.com>
2018-10-28 15:30:05 -0400

    Fix MHL agent

--- clients/clients.php
+++ clients/clients.php
@@ -1,14 +1,38 @@
 <?php
   // Copyright 2018 BACnet Gateway.  All rights reserved.
 
-  $sIp = $_SERVER['REMOTE_ADDR'];
-  $sFwdIp = isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : '';
+  $aOptions =
+  [
+    'REMOTE_ADDR',
+    'HTTP_X_FORWARDED_FOR',
+    'REMOTE_PORT',
+    'HTTP_REFERER',
+    'REQUEST_METHOD',
+    'QUERY_STRING',
+    'REQUEST_URI',
+    'REQUEST_TIME'
+  ];
+
+  $sArgs = '';
+
+  foreach( $aOptions as $sOption )
+  {
+    $sArgs .= makeArg( $sOption );
+  }
 
   // Format command
-  $command = SUDO . quote( getenv( 'PYTHON' ) ) . ' clients/clients.py 2>&1 -i ' . quote( $sIp ) . ' -f ' . quote( $sFwdIp );
+  $command = SUDO . quote( getenv( 'PYTHON' ) ) . ' clients/clients.py 2>&1 ' . $sArgs;
 
   // Execute command
   error_log( '==> command=' . $command );
   exec( $command, $output, $status );
   error_log( '==> output=' . print_r( $output, true ) );
+
+  /////////////////////////
+
+  function makeArg( $sOption )
+  {
+    $sArg = quote( ( isset( $_SERVER[$sOption] ) ) ? $_SERVER[$sOption] : '' );
+    return ' --' . $sOption . ' ' . quote( ( isset( $_SERVER[$sOption] ) ) ? $_SERVER[$sOption] : '' );
+  }
 ?>
--- clients/clients.py
+++ clients/clients.py
@@ -2,7 +2,7 @@
 
 import os
 import sqlite3
-import time
+import datetime
 
 
 
@@ -30,11 +30,18 @@ def open_db():
         cur.executescript('''
             CREATE TABLE IF NOT EXISTS Clients (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                remote_address TEXT,
-                http_x_forwarded_for TEXT,
+                REMOTE_ADDR TEXT,
+                HTTP_X_FORWARDED_FOR TEXT,
+                REMOTE_PORT TEXT,
+                HTTP_REFERER TEXT,
+                REQUEST_METHOD TEXT,
+                QUERY_STRING TEXT,
+                REQUEST_URI TEXT,
                 access_count INTEGER,
-                first_access_timestamp INTEGER,
-                last_access_timestamp INTEGER
+                access_time_1 TEXT,
+                access_time_n TEXT,
+                REQUEST_TIME_1 INTEGER,
+                REQUEST_TIME_n INTEGER
             );
         ''')
 
@@ -46,21 +53,52 @@ def open_db():
 def track_client():
 
     # Test whether entry already exists
-    cur.execute( 'SELECT id FROM Clients WHERE remote_address=? AND http_x_forwarded_for=?', ( args.ip, args.fwd_ip ) )
+    cur.execute( 'SELECT id FROM Clients WHERE REMOTE_ADDR=? AND HTTP_X_FORWARDED_FOR=? AND REMOTE_PORT=?', ( args.REMOTE_ADDR, args.HTTP_X_FORWARDED_FOR, args.REMOTE_PORT ) )
     row = cur.fetchone()
 
-    # Get current time
-    timestamp = int( time.time() )
+    access_time = datetime.datetime.fromtimestamp( args.REQUEST_TIME ).strftime('%Y-%m-%d %H:%M:%S')
 
     if row:
 
         # Entry exists; update it
-        cur.execute( 'UPDATE Clients SET access_count=access_count+1, last_access_timestamp=? WHERE id=?', ( timestamp, row[0] ) )
+        cur.execute( 'UPDATE Clients SET access_count=access_count+1, access_time_n=?, REQUEST_TIME_n=? WHERE id=?', ( access_time, args.REQUEST_TIME, row[0] ) )
 
     else:
 
         # Entry does not exist; insert it
-        cur.execute( 'INSERT INTO Clients ( remote_address, http_x_forwarded_for, access_count, first_access_timestamp, last_access_timestamp ) VALUES (?,?,?,?,?)', ( args.ip, args.fwd_ip, 1, timestamp, timestamp ) )
+        cur.execute( '''
+
+            INSERT INTO Clients (
+                REMOTE_ADDR,
+                HTTP_X_FORWARDED_FOR,
+                REMOTE_PORT,
+                HTTP_REFERER,
+                REQUEST_METHOD,
+                QUERY_STRING,
+                REQUEST_URI,
+                access_count,
+                access_time_1,
+                access_time_n,
+                REQUEST_TIME_1,
+                REQUEST_TIME_n
+            )
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
+
+            ''', (
+                args.REMOTE_ADDR,
+                args.HTTP_X_FORWARDED_FOR,
+                args.REMOTE_PORT,
+                args.HTTP_REFERER,
+                args.REQUEST_METHOD,
+                args.QUERY_STRING,
+                args.REQUEST_URI,
+                1,
+                access_time,
+                access_time,
+                args.REQUEST_TIME,
+                args.REQUEST_TIME
+                )
+        )
 
     conn.commit()
 
@@ -69,9 +107,15 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser( description='Track BACnet Gateway clients' )
-    parser.add_argument( '-i', dest='ip', help='Remote IP address' )
-    parser.add_argument( '-f', dest='fwd_ip', help='Forwarded for IP address' )
-    parser.add_argument( '-r', dest='remove', action='store_true', help='Remove database' )
+    parser.add_argument( '--REMOTE_ADDR', dest='REMOTE_ADDR' )
+    parser.add_argument( '--HTTP_X_FORWARDED_FOR', dest='HTTP_X_FORWARDED_FOR' )
+    parser.add_argument( '--REMOTE_PORT', dest='REMOTE_PORT' )
+    parser.add_argument( '--HTTP_REFERER', dest='HTTP_REFERER' )
+    parser.add_argument( '--REQUEST_METHOD', dest='REQUEST_METHOD' )
+    parser.add_argument( '--QUERY_STRING', dest='QUERY_STRING' )
+    parser.add_argument( '--REQUEST_URI', dest='REQUEST_URI' )
+    parser.add_argument( '--REQUEST_TIME', dest='REQUEST_TIME', type=int )
+    parser.add_argument( '-r', dest='remove', action='store_true' )
     args = parser.parse_args()
 
     conn, cur = open_db()
