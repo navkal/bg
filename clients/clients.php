1e823cba3372b08f57705cf29d4906a1227a7537
navkal <navkal@hotmail.com>
2018-10-28 23:56:35 -0400

    Clients database

--- clients/clients.php
+++ clients/clients.php
@@ -1,28 +1,31 @@
 <?php
   // Copyright 2018 BACnet Gateway.  All rights reserved.
 
-  $aOptions =
-  [
-    'REMOTE_ADDR',
-    'HTTP_X_FORWARDED_FOR',
-    'HTTP_REFERER',
-    'REQUEST_TIME'
-  ];
-
-  $sArgs = '';
-
-  foreach( $aOptions as $sOption )
+  if ( $_SERVER['REMOTE_ADDR'] != '127.0.0.1' )
   {
-    $sArgs .= makeArg( $sOption );
+    $aOptions =
+    [
+      'REMOTE_ADDR',
+      'HTTP_X_FORWARDED_FOR',
+      'HTTP_REFERER',
+      'REQUEST_TIME'
+    ];
+
+    $sArgs = '';
+
+    foreach( $aOptions as $sOption )
+    {
+      $sArgs .= makeArg( $sOption );
+    }
+
+    // Format command
+    $command = SUDO . quote( getenv( 'PYTHON' ) ) . ' clients/clients.py 2>&1 ' . $sArgs;
+
+    // Execute command
+    error_log( '==> command=' . $command );
+    exec( $command, $output, $status );
+    error_log( '==> output=' . print_r( $output, true ) );
   }
-
-  // Format command
-  $command = SUDO . quote( getenv( 'PYTHON' ) ) . ' clients/clients.py 2>&1 ' . $sArgs;
-
-  // Execute command
-  error_log( '==> command=' . $command );
-  exec( $command, $output, $status );
-  error_log( '==> output=' . print_r( $output, true ) );
 
   /////////////////////////
 
