6cd3f4213af95d0e8f12c6d7713e655503e29a4c
navkal <navkal@hotmail.com>
2018-11-15 17:54:46 -0500

    Weather station interface

--- ws.php
+++ ws.php
@@ -1,8 +1,7 @@
 <?php
   // Copyright 2018 Building Energy Gateway.  All rights reserved.
 
-  $sKey = isset( $_REQUEST['key'] ) ? $_REQUEST['key'] : null;
-
+  $sProperty = isset( $_REQUEST['property'] ) ? $_REQUEST['property'] : 'temperature';
 
   $sKey = 'temperature';  // <-- fake
 
