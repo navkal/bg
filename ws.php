4e45e5b226e4048c9b5fd9c0f37a170ca4806208
navkal <navkal@hotmail.com>
2018-11-15 15:14:13 -0500

    Weather station interface

--- ws.php
+++ ws.php
@@ -2,5 +2,19 @@
   // Copyright 2018 Building Energy Gateway.  All rights reserved.
 
   $sKey = isset( $_REQUEST['key'] ) ? $_REQUEST['key'] : null;
-  echo json_encode( $sKey );
-?>
+
+
+  $sKey = 'temperature';  // <-- fake
+
+  // Issue request to web service
+  $curl = curl_init();
+  curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+  curl_setopt( $curl, CURLOPT_URL, "https://owc.enterprise.earthnetworks.com/Data/GetData.ashx?dt=o&pi=3&si=ANSHS&locstr=42.65998,-71.15531&units=english&verbose=true" );
+  $aResult = json_decode( json_encode( json_decode( curl_exec( $curl ) ) ), true );
+
+  // Extract requested item
+  $sTemp = $aResult['observation'][$sKey];
+
+  // Return JSON
+  $sEcho = json_encode( $sTemp );
+  echo $sEcho;
