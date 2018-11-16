53dcb5a6f03c85ba5c732f6ade4f389590d6b8b8
navkal <navkal@hotmail.com>
2018-11-15 23:56:31 -0500

    Weather station interface

--- ws.php
+++ ws.php
@@ -1,19 +1,89 @@
 <?php
   // Copyright 2018 Building Energy Gateway.  All rights reserved.
 
-  $sProperty = isset( $_REQUEST['property'] ) ? $_REQUEST['property'] : 'temperature';
+  // Table of weather stations
+  $aStations =
+  [
+    'ahs' =>
+    [
+      'label' => 'Andover High School Weather Station',
+      'url' => 'https://owc.enterprise.earthnetworks.com/Data/GetData.ashx?dt=o&pi=3&si=ANSHS&locstr=42.65998,-71.15531&units=english&verbose=true'
+    ]
+  ];
 
-  $sKey = 'temperature';  // <-- fake
+  // Get request arguments
+  $sStation = $_REQUEST['station'];
+  $sProperty = isset( $_REQUEST['property'] ) ? $_REQUEST['property'] : '';
 
-  // Issue request to web service
-  $curl = curl_init();
-  curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-  curl_setopt( $curl, CURLOPT_URL, "https://owc.enterprise.earthnetworks.com/Data/GetData.ashx?dt=o&pi=3&si=ANSHS&locstr=42.65998,-71.15531&units=english&verbose=true" );
-  $aResult = json_decode( json_encode( json_decode( curl_exec( $curl ) ) ), true );
+  $sMessage = '';
+  $sLabel = '';
 
-  // Extract requested item
-  $sTemp = $aResult['observation'][$sKey];
+  // Look for requested weather station in table
+  if ( isset( $aStations[$sStation] ) )
+  {
+    // Set label for requested weather station
+    $sLabel = $aStations[$sStation]['label'];
+
+    // Issue request to web service
+    $curl = curl_init();
+    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $curl, CURLOPT_URL, $aStations[$sStation]['url'] );
+    $aRsp = json_decode( json_encode( json_decode( curl_exec( $curl ) ) ), true );
+
+    // Determine result: either requested property or full response
+    if ( $sProperty )
+    {
+      // Caller requested a property; search for it in the response
+
+      if ( array_key_exists( $sProperty, $aRsp ) )
+      {
+        // Requested property found at top level of response
+        $property = $aRsp[$sProperty];
+      }
+      else
+      {
+        foreach ( $aRsp as $aProps )
+        {
+          if ( array_key_exists( $sProperty, $aProps ) )
+          {
+            // Requested property found at second level of response
+            $property = $aProps[$sProperty];
+            break;
+          }
+        }
+      }
+
+      if ( ! isset( $property ) )
+      {
+        // Requested property not found
+        $property = '';
+        $sMessage = "Property '" . $sProperty . "' not found";
+      }
+    }
+    else
+    {
+      // Caller did not request a property; return full response
+      $property = $aRsp;
+    }
+  }
+  else
+  {
+    // Requested weather station not found
+    $property = '';
+    $sMessage = "Station '" . $sStation . "' not found";
+  }
+
+  // Load results into structure
+  $aResult =
+  [
+    'success' => ( $sMessage == '' ),
+    'message' => $sMessage,
+    'station' => $sStation,
+    'label' => $sLabel,
+    'requestedProperty' => $sProperty,
+    $sProperty => $property
+  ];
 
   // Return JSON
-  $sEcho = json_encode( $sTemp );
+  $sEcho = json_encode( $aResult );
   echo $sEcho;
