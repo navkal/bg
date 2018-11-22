5ca9e903f078d8e87a301f494eb7c6a48f244f8b
navkal <navkal@hotmail.com>
2018-11-21 20:54:24 -0500

    Weather station interface

--- fm.php
+++ fm.php
@@ -1,6 +1,17 @@
 <?php
   // Copyright 2018 Building Energy Gateway.  All rights reserved.
 
+  $aFacilityMap = [];
+
+  foreach ( $g_aFacilities as $sFacType => $aFacsOfType )
+  {
+    $aFacilityMap[$sFacType] = [];
+    foreach ( $aFacsOfType as $sFacName => $aFac )
+    {
+      array_push( $aFacilityMap[$sFacType], $sFacName );
+    }
+  }
+
   // Echo result
-  echo json_encode( $g_aFacilities );
+  echo json_encode( $aFacilityMap );
 ?>
